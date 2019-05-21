26cf33cdb4cfaf9fbeccf8fce00509eed15e3a45
Joseph Woodley <josephjwoodley@gmail.com>
2019-05-20 22:51:24 -0400

    update routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 var coolRouter = require('./routes/users');
 
 var app = express();
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
 app.use('/users/cool', coolRouter);
 
 // catch 404 and forward to error handler
